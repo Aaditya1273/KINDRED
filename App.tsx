51e57a7917128706ca3e2f54be78ec630159dca9
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-03-12 20:17:43 +0530

    mobile fixes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
 import { registerRootComponent } from 'expo';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { ErrorBoundary } from './src/components/ErrorBoundary';
 
-registerRootComponent(AppNavigator);
+const App = () => (
+  <ErrorBoundary>
+    <AppNavigator />
+  </ErrorBoundary>
+);
+
+registerRootComponent(App);
